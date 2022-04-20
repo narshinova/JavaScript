8810f334e97db195ef5216a0b17da385540333d1
Your Name <you@example.com>
2022-04-20 11:49:42 +0300

    HW_2

---
+++ test2.js
@@ -0,0 +1,25 @@
+const checkAge = function (age) {
+    Number(age)
+
+    if (typeof (age) != Number)
+        console.log("Error")
+    else {
+
+        if (age_1 < age_2) {
+            console.log("You don’t have access cause your age is " + age_1 + " It’s less then " + age_2)
+        }
+        else if (age_1 >= age_2 && age_1 < age_3) {
+            console.log("Welcome!")
+        }
+        else if (age_1 > age_3) {
+            console.log("Keep calm and look Culture channel")
+        }
+        else {
+            console.log("Technical work")
+        }
+    }
+}
+checkAge(17)
+checkAge(18)
+checkAge(61)
+checkAge('2')
